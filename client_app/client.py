d75a03be69eedae87e36184e984c5ef3cee1d873
Alex Khrystoforov <alexkhrystoforov@gmail.com>
2021-11-07 23:34:42 +0200

    Add files via upload

--- client_app/client.py
+++ client_app/client.py
@@ -9,12 +9,15 @@ if os.environ.get('http_proxy'):
     del os.environ['http_proxy']
 
 
-def post_method(server_port):
-    with grpc.insecure_channel(server_port) as channel:
+
+def post_method():
+    with grpc.insecure_channel(servers_ports[0]) as channel:
         stub = user_pb2_grpc.UserServiceStub(channel)
         name = str(input('enter your name:'))
-        response = stub.append(user_pb2.UserPostRequest(name=name))
+        w = int(input('write concern:'))
+        response = stub.append(user_pb2.UserPostRequest(name=name, write_concern=w))
         print('response is', response.success)
+        channel.close()
 
 
 def get_method(server_port):
@@ -44,15 +47,18 @@ def select_server():
 
 
 # 51 - master, 52-53 - secondaries. POST only for master, GET for any
-# servers_ports = ["50051", "50052", "50053"]
-servers_ports = ["localhost:50051", "localhost:50052", "localhost:50053"]
+servers_ports = ["localhost:50051", "localhost:50052", "localhost:50053"] # failed connect grpc_status=14
+# servers_ports = ["127.0.0.1:50051", "localhost:50052", "localhost:50053"] # failed connect grpc_status=14
+# servers_ports = ["172.17.0.2:50051", "localhost:50052", "localhost:50053"] # failed connect grpc_status=14
+# servers_ports = [":50051", "localhost:50052", "localhost:50053"] #DNS error
+# servers_ports = ['50051']
 
 
 def run():
     method_key = select_method()
 
     if method_key == '1':
-        post_method(servers_ports[0]), run()
+        post_method(), run()
 
     elif method_key == '2':
         server_key = select_server()
