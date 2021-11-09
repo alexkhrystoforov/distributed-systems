bddcf04fe273fe23fea3c2308612b3204fcbc11c
Alex Khrystoforov <alexkhrystoforov@gmail.com>
2021-11-09 17:41:03 +0200

    Add files via upload

--- master/master.py
+++ master/master.py
@@ -5,17 +5,18 @@ import master_to_secondary_pb2_grpc as master_to_secondary_pb2_grpc
 import master_to_secondary_pb2 as master_to_secondary_pb2
 
 from concurrent import futures
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 import threading
 
 import time
-from datetime import datetime
+import os
 import grpc
 import logging
 
-from joblib import Parallel, delayed
-import multiprocessing
-import os
+if os.environ.get('https_proxy'):
+ del os.environ['https_proxy']
+if os.environ.get('http_proxy'):
+ del os.environ['http_proxy']
+
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
@@ -27,22 +28,27 @@ class UserServicer(user_pb2_grpc.UserServiceServicer):
     def append(self, request, context):
         global ACK_counter
         save(request)
-        #Parallel(n_jobs=2, require='sharedmem')(delayed(replicate_method)(port) for port in servers_ports)
 
         t2 = threading.Thread(target=replicate_method, args=[request.msg, servers_ports[0]])
         t3 = threading.Thread(target=replicate_method, args=[request.msg,servers_ports[1]])
         t2.start()
         t3.start()
 
-        while True:
+        n = 0
+        # wait for ACK for 3sec
+        while n <= 3:
+            time.sleep(1)
+
             if sum(ACK_counter) >= request.write_concern - 1:
                 print('START ACK TO CLIENT')
                 return user_pb2.UserPostResponse(success=f'SUCCESS: {request}')
-        # else:
-        #     delete_last_save()
-        #     return user_pb2.UserPostResponse(success='Sorry, POST method was failed.'
-        #                                              f'write concern is {request.write_concern},'
-        #                                              f'number of ACK from secondaries is {sum(ACK_counter)}')
+            n += 1
+
+        else:
+            delete_last_save()
+            return user_pb2.UserPostResponse(success='Sorry, POST method was failed.'
+                                                     f'write concern is {request.write_concern},'
+                                                     f'number of ACK from secondaries is {sum(ACK_counter)}')
 
     def get(self, request, context):
         if request.get:
@@ -71,6 +77,7 @@ def replicate_method(request_msg,port):
 def delete_last_save():
     if all_msg:
         del all_msg[-1]
+        print('Delete data:', all_msg[-1])
 
 
 def save(request):
