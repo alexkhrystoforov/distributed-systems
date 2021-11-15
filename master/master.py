9cc3ac4e1c3426103bba504a82d78c85905918b2
Alex Khrystoforov <alexkhrystoforov@gmail.com>
2021-11-15 13:31:37 +0200

    Add files via upload

--- master/master.py
+++ master/master.py
@@ -1,120 +1,80 @@
 import user_pb2_grpc as user_pb2_grpc
 import user_pb2 as user_pb2
-
 import master_to_secondary_pb2_grpc as master_to_secondary_pb2_grpc
 import master_to_secondary_pb2 as master_to_secondary_pb2
 
 from concurrent import futures
 import threading
-import multiprocessing
 import time
-import os
 import grpc
 import logging
-
-if os.environ.get('https_proxy'):
- del os.environ['https_proxy']
-if os.environ.get('http_proxy'):
- del os.environ['http_proxy']
 
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
-ACK_counter = {}
-stop_threads = False
 all_msg = []
+all_id = []
+server_down = False
+servers_ports = ["localhost:50052", "localhost:50053"]
 
 
 class UserServicer(user_pb2_grpc.UserServiceServicer):
     def append(self, request, context):
-        global ACK_counter
+
+        if request.write_concern > len(servers_ports)+1:
+            return user_pb2.UserPostResponse(success='Post request is declined.'
+                                                         'write concern is too big,'
+                                                         f'the biggest possible w={len(servers_ports)+1}')
+
+        # if server_down and request.write_concern == len(servers_ports) + 1:
+        #     return user_pb2.UserPostResponse(success='Post request is declined.'
+        #                                              'something wrong with servers')
 
         save(request)
+        latch = CountDownLatch(request.write_concern - 1)
 
-        t2 = threading.Thread(target=replicate_method, args=[request.msg, servers_ports[0], ])
-        t3 = threading.Thread(target=replicate_method, args=[request.msg,servers_ports[1], ])
+        for port in servers_ports:
+            t = threading.Thread(target=replicate_method, args=[request.msg, port, latch])
+            t.start()
 
-        t2.start()
-        t3.start()
+        latch.a_wait()
 
-
-        n = 0
-        # wait for ACK for 3sec
-        while n <= 3:
-
-            if sum(ACK_counter.values()) >= request.write_concern - 1:
-                print('START ACK TO CLIENT')
-                return user_pb2.UserPostResponse(success=f'SUCCESS: {request}')
-            time.sleep(0.5)
-            n += 1
-
-        else:
-            # stop replication
-            # global stop_threads
-            # stop_threads = True
-            # for t in [t2,t3]:
-            #     if t.is_alive():
-            #         t.join()
-
-            delete_last_save()
-            delete_secondary_last_save()
-
-            return user_pb2.UserPostResponse(success='Sorry, POST method was failed.'
-                                                     f'write concern is {request.write_concern},'
-                                                     f'number of ACK from secondaries is {sum(ACK_counter.values())}')
+        return user_pb2.UserPostResponse(success=f'SUCCESS: {request}')
 
     def get(self, request, context):
         if request.get:
             print("Process get request...")
             return user_pb2.UserGetResponse(msg=all_msg)
 
-#
-# def stop_thread():
-#     global stop_threads
-#     stop_threads = True
 
-
-def replicate_method(request_msg, port):
+def replicate_method(request_msg, port, latch):
+    global all_id
+    global server_down
 
     with grpc.insecure_channel(port) as channel:
-
         if grpc_server_on(channel):
-            print(f'Replication to secondary with port {port}')
-            print(f'name for {port}', request_msg)
+            print(f'Start replication to secondary with port {port}')
             stub = master_to_secondary_pb2_grpc.MasterServiceStub(channel)
-            response = stub.replicate(master_to_secondary_pb2.ReplicateRequest(msg=request_msg))
-            channel.close()
-            print(f'ACK from port {port}', response.ACK)
-            global ACK_counter
-            ACK_counter[port] = response.ACK
-
-        else:
-            print(f'server {port} is dead')
-            ACK_counter[port] = False
-
-
-def delete_last_save():
-    if all_msg:
-        print('Delete data:', all_msg[-1])
-        del all_msg[-1]
-
-
-def delete_secondary_last_save():
-    # get all ports with successful replication
-    ports = list(port for port, ACK_status in ACK_counter.items() if ACK_status == True)
-
-    for port in ports:
-        with grpc.insecure_channel(port) as channel:
-            if grpc_server_on(channel):
-                stub = master_to_secondary_pb2_grpc.MasterServiceStub(channel)
-                response = stub.deletelastmsg(master_to_secondary_pb2.DeleteLastMsgRequest(delete=True))
-                print(f'deleted last replicated msg on secondary with port {port}', response.delete)
-                channel.close()
+            response = stub.replicate(master_to_secondary_pb2.ReplicateRequest(msg=request_msg, id=all_id[-1]))
+            if response.ACK:
+                print(f'replicated to {port}')
+                latch.count_down()
+        # else:
+        #     print(f'server with port {port} is not available')
+        #     latch.count_down()
+        #     server_down = True
 
 
 def save(request):
+    global all_id
+    global all_msg
+
     all_msg.append(request.msg)
-    print('Save new data:', all_msg)
+    if not all_id:
+        all_id.append(1)
+    else:
+        all_id.append(all_id[-1] + 1)
+    print('Save new data:', all_msg, all_id)
 
 
 def grpc_server_on(channel) -> bool:
@@ -126,7 +86,23 @@ def grpc_server_on(channel) -> bool:
         return False
 
 
-servers_ports = ["localhost:50052", "localhost:50053"]
+class CountDownLatch(object):
+    def __init__(self, count=1):
+        self.count = count
+        self.lock = threading.Condition()
+
+    def count_down(self):
+        self.lock.acquire()
+        self.count -= 1
+        if self.count <= 0:
+            self.lock.notifyAll()
+        self.lock.release()
+
+    def a_wait(self):
+        self.lock.acquire()
+        while self.count > 0:
+            self.lock.wait()
+        self.lock.release()
 
 
 def grpc_server():
