3c7ac524580588a6d934020006c922b2b2b64a1c
Alex Khrystoforov <alexkhrystoforov@gmail.com>
2021-12-03 14:10:25 +0200

    Add files via upload

--- client_app/client.py
+++ client_app/client.py
@@ -4,8 +4,7 @@ import argparse
 
 
 def post_method(msg, w):
-    # with grpc.insecure_channel("localhost:50051", options=options) as channel:
-    with grpc.insecure_channel("localhost:50051") as channel:
+    with grpc.insecure_channel("0.0.0.0:50051") as channel:
         if grpc_server_on(channel):
             stub = user_pb2_grpc.UserServiceStub(channel)
             response = stub.append(user_pb2.UserPostRequest(msg=msg, write_concern=w))
@@ -16,13 +15,13 @@ def post_method(msg, w):
 
 
 def get_method(server_port):
-    with grpc.insecure_channel('localhost:'+server_port) as channel:
+    with grpc.insecure_channel('0.0.0.0:'+server_port) as channel:
         if grpc_server_on(channel):
             stub = user_pb2_grpc.UserServiceStub(channel)
             response = stub.get(user_pb2.UserGetRequest(get=True))
             print('response is:', response)
         else:
-            print('localhost:'+server_port, 'is not available')
+            print('0.0.0.0:'+server_port, 'is not available')
 
 
 def grpc_server_on(channel) -> bool:
